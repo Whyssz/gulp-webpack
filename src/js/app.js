d06c0f359559fbba828335ef144105bfa785bce7
Dmitry <dragoniiix@mail.ru>
2022-04-21 16:47:00 +0300

    building settings

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from './modules/functions.js';
+
+flsFunctions.isWeb();
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+//
+// const swiper = new Swiper('.swiper', {
+//
+// });
